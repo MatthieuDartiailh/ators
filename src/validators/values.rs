e5a67c32a401f2d4aabbc770c7d579ac1cb43918
MatthieuDartiailh <m.dartiailh@gmail.com>
2025-10-09 21:42:11 -0600

    implement simple value validation

--- src/validators/values.rs
+++ src/validators/values.rs
@@ -1,7 +1,7 @@
 ///
 use pyo3::{
     Bound, Py, PyAny, PyResult, Python, pyclass,
-    types::{PyFrozenSet, PyString},
+    types::{PyAnyMethods, PyFrozenSet, PyFrozenSetMethods, PyString},
 };
 
 #[pyclass(frozen)]
@@ -9,9 +9,9 @@ pub enum ValueValidator {
     #[pyo3(constructor = (values))]
     Enum { values: Py<PyFrozenSet> },
     #[pyo3(constructor = (items))]
-    TupleItems { items: Vec<ValueValidator> },
+    TupleItems { items: Vec<Vec<ValueValidator>> },
     #[pyo3(constructor = (item))]
-    SequenceItems { item: Py<ValueValidator> },
+    SequenceItems { item: Vec<ValueValidator> },
     #[pyo3(constructor = (callable))]
     CallMemberObjectValue { callable: Py<PyAny> },
     #[pyo3(constructor = (meth_name))]
@@ -29,7 +29,68 @@ impl ValueValidator {
         object: Option<&Bound<'py, crate::core::BaseAtors>>,
         value: &Bound<'py, PyAny>,
     ) -> PyResult<()> {
-        Ok(()) // XXX
+        match self {
+            Self::Enum { values } => {
+                if values
+                    .bind(value.py())
+                    .contains(value)
+                    .unwrap_or(false)
+                {
+                    Ok(())
+                } else {
+                    Err(pyo3::exceptions::PyValueError::new_err(format!(
+                        "Value {} not in enum {}",
+                        value.repr()?,
+                        values.bind(value.py()).repr()?
+                    )))
+                }
+            }
+            Self::TupleItems { items } => {
+                // The number of items is checked by the type validator and
+                // the validator ensure this value validator is only ever used
+                // with the appropriate type validator
+                for (item_res, item_validators) in value.try_iter()?.zip(items.iter()) {
+                    let item = item_res?;
+                    for item_validator in item_validators.iter() {
+                        item_validator.validate_value(member, object, &item)?
+                    };
+                }
+                Ok(())
+            }
+            Self::SequenceItems { item } => {
+                for el_res in value.try_iter()? {
+                    let el = el_res?;
+                    for ival in item.iter() {
+                        ival.validate_value(member, object, &el)?;
+                    }
+                }
+                Ok(())
+            }
+            Self::CallMemberObjectValue { callable } => callable
+                .bind(value.py())
+                .call1(
+                    (
+                        member.ok_or(pyo3::exceptions::PyRuntimeError::new_err(
+                            "Cannot use CallMemberObjectValue validation when validator is not linked to a member."
+                        ))?,
+                        object.ok_or(
+                            pyo3::exceptions::PyTypeError::new_err(
+                                "Cannot use CallMemberObjectValue validation when validator is not linked to a member."
+                            )
+                        )?,
+                        value,
+                    ),
+                )
+                .map(|_| ()),
+            Self::ObjectMethod { meth_name } => object
+                .ok_or(pyo3::exceptions::PyTypeError::new_err(
+                    "Cannot use ObjectMethod validation when validator is not linked to a member.",
+                ))?
+                .call_method1(meth_name, (member.ok_or(pyo3::exceptions::PyRuntimeError::new_err(
+                    "Cannot use ObjectMethod validation when validator is not linked to a member."
+                ))?, value))
+                .map(|_| ()),
+        }
     }
 }
 
@@ -43,7 +104,7 @@ impl Clone for ValueValidator {
                 items: items.to_vec(),
             },
             Self::SequenceItems { item } => Self::SequenceItems {
-                item: item.clone_ref(py),
+                item: item.to_vec(),
             },
             Self::CallMemberObjectValue { callable } => Self::CallMemberObjectValue {
                 callable: callable.clone_ref(py),
