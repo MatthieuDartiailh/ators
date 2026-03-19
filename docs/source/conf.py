14570c6be3ccc114df8ba1b29ff4ef8123dd69d9
MatthieuDartiailh <m.dartiailh@gmail.com>
2026-03-19 13:10:07 +0100

    docs: format conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Ators'
-copyright = '2026, Matthieu C Dartiailh'
-author = 'Matthieu C Dartiailh'
-release = '0.0.1'
+project = "Ators"
+copyright = "2026, Matthieu C Dartiailh"
+author = "Matthieu C Dartiailh"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,13 +23,12 @@ extensions = [
 
 autoapi_dirs = ["../../python/ators"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
